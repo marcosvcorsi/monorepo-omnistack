c4259671848b4753cad722cd4f95c7d8f0d2962b
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-02 16:13:58 -0300

    Projeto terminado

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+// import api from '@omnirepo/axios-config';
+
+const App: React.FC = () => {
+  useEffect(() => {
+    // api.get('/').then((response) => {
+    //   console.log(response);
+    // });
+  }, []);
+
+  return <h1>Hello World</h1>;
+};
+
+export default App;
